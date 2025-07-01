0bea246c7969934c151c18597e20c0fc85e32179
wows <vishnuartham@gmail.com>
2025-07-01 20:19:36 +0530

    second

---
+++ iris_data.py
@@ -0,0 +1,9 @@
+from sklearn.datasets import load_iris
+import pandas as pd
+
+iris = load_iris()
+df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+df['target'] = iris.target
+
+df.to_csv('iris_data.csv', index=False)
+print("Iris dataset saved as 'iris_data.csv'")
